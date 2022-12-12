3e12b50657eb29e5d7d6e2934b981c3c9b72b935
thuy <109534863+Thuy2y2c@users.noreply.github.com>
2022-12-12 15:12:28 +0700

    Create oke.lua

---
+++ oke.lua
@@ -0,0 +1,8 @@
+local UI = urlLoad("https://raw.githubusercontent.com/Thuy2y2c/heavily-overused-roblox-hub-gui/main/ui.lua")
+local themeManager = urlLoad("https://raw.githubusercontent.com/Thuy2y2c/heavily-overused-roblox-hub-gui/main/themeManager.lua")
+
+local metadata = urlLoad("https://raw.githubusercontent.com/Thuy2y2c/heavily-overused-roblox-hub-gui/main/metadata.lua") -- metadata...
+local httpService = game:GetService('HttpService')
+
+local framework, scrollHandler, network
+local counter = 0
